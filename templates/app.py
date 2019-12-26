9a0df2aaa6065998ea4a07033c6514efc172068d
Thomas Ruble <sketchbang@gmail.com>
2019-12-26 17:20:32 -0600

    add app template

--- templates/app.py
+++ templates/app.py
@@ -1,85 +1,201 @@
 import copy
 
 def generate_config(ctx):
-    props = ctx.properties
     app_id = ctx.env['name']
-    parent_node = props['parent_node']
+    
+    props = ctx.properties
 
-    # Create Root Folder
-    root_parent_type = parent_node.get('type')
-    root_parent_id = parent_node.get('id')
-    root_parent = f'{root_parent_type}s/{root_parent_id}'
-    folder_resource = {
-        'name': app_id,
-        'type': 'gcp-types/cloudresourcemanager-v2:folders',
-        'properties': {
-            'parent': root_parent,
-            'displayName': app_id
-        }
+    root_node = props['root_node']
+    dev_node = props['dev_node']
+    prd_node = props['prd_node']
+    billing_account_id = props['billing_account_id']
+
+    envs = [
+        {'name': 'dev', 'id': dev_node},
+        {'name': 'prd', 'id': prd_node}
+    ]
+    project_configs = make_project_configs(app_id, envs)
+    
+    resources = []
+
+    # Add projects & associated billing accounts
+    resources.extend(
+        make_projects_and_billing_accounts(project_configs, billing_account_id)
+    )
+
+    # Enable services (APIs) for projects
+    for project_config in project_configs:
+        project_id = project_config['id']
+        services = project_config['services']
+        enabled_services_resources = make_enabled_services(project_id, services)
+        resources.extend(enabled_services_resources)
+
+    # Delete default network
+    for project_config in project_configs:
+        # TODO
+        if False and project_config['delete_network']:
+            delete_network_resources = make_delete_default_network(project_config)
+            resources.extend(delete_network_resources)
+    
+    return {
+        'resources': resources,
     }
-    root_id = f'$(ref.{app_id}.name)'
 
-    # Create Env Folders
-    dev_folder = app_id + '-DEV'
-    folder_dev_resource = {
-        'name': dev_folder,
-        'type': 'gcp-types/cloudresourcemanager-v2:folders',
+
+def make_project_configs(app_id, envs):
+    project_configs = []
+    services = []
+    for env in envs:
+        env_name = env['name']
+        env_node_id = env['id']
+        configs = [
+            {
+                'id': '{}-{}'.format(app_id, env_name), 
+                'parent_id': env_node_id, 
+                'services': services,
+                'delete_network': True
+            },
+            {
+                'id': '{}-net-{}'.format(app_id, env_name), 
+                'parent_id': env_node_id, 
+                'services': services,
+                'delete_network': True
+            },
+            {
+                'id': '{}-mon-{}'.format(app_id, env_name), 
+                'parent_id': env_node_id, 
+                'services': services,
+                'delete_network': True
+            },
+        ]
+        project_configs.extend(configs)
+
+    return project_configs
+
+
+def make_projects_and_billing_accounts(project_configs, billing_account_id):
+    def make_project(project_config):
+        parent_id = project_config['parent_id']
+        project_id = project_config['id']
+        return {
+
+            'name': project_id,
+            'type': 'cloudresourcemanager.v1.project',
+            'properties': {
+                'name': project_id,
+                'projectId': project_id,
+                'parent': {
+                    'type': 'folder',
+                    'id': str(parent_id)
+                }
+            }
+        }
+
+    def make_billing_account(project_id):
+        return {
+            'name': get_billing_account_name(project_id),
+            'type': 'deploymentmanager.v2.virtual.projectBillingInfo',
+            'metadata': {
+                'dependsOn': [project_id]
+            },
+            'properties': {
+                'name': 'projects/{}'.format(project_id),
+                'billingAccountName': 'billingAccounts/{}'.format(billing_account_id)
+            }
+        }
+    
+    resources = []
+    for project_config in project_configs:
+        resources.extend([
+            make_project(project_config),
+            make_billing_account(project_config['id'])
+        ])
+    
+    return resources
+
+
+def get_billing_account_name(project_id):
+    return '{}-billing'.format(project_id)
+
+
+def make_billing_accounts(project_ids, billing_account_id):
+    def make_billing_account(project_id):
+        return {
+            'name': get_billing_account_name(project_id),
+            'type': 'deploymentmanager.v2.virtual.projectBillingInfo',
+            'metadata': {
+                'dependsOn': [project_id]
+            },
+            'properties': {
+                'name': 'projects/{}'.format(project_id),
+                'billingAccountName': 'billingAccounts/{}'.format(billing_account_id)
+            }
+        }
+    return [make_billing_account(project_id) for project_id in project_ids]
+
+
+def make_enabled_services(project_id, services):
+    dependsOn = [project_id, get_billing_account_name(project_id)]
+    resources = []
+    for service in services:
+        name = '{}-{}'.format(project_id, service)
+        consumerId = 'project:{}'.format(project_id)
+        resource = {
+            'name': name,
+            'type': 'deploymentmanager.v2.virtual.enableService',
+            'metadata': {
+                'dependsOn': dependsOn[:]
+            },
+            'properties': {
+                'consumerId': consumerId,
+                'serviceName': service
+            }
+        }
+        dependsOn.append(name)
+        resources.append(resource)
+
+    return resources
+
+
+def make_delete_default_network(project_config):
+    default_firewalls = [
+        'default-allow-icmp',
+        'default-allow-internal',
+        'default-allow-rdp',
+        'default-allow-ssh',
+    ]
+
+    project_id = project_config['id']
+    services = project_config['services']
+
+    def make_delete_firewall(firewall_name):
+        return {
+            'name': '{}-firewall-delete-{}'.format(project_id, firewall_name),
+            'action': 'gcp-types/compute-v1:compute.firewalls.delete',
+            'metadata': {
+                'dependsOn': services
+            },
+            'properties': {
+                'firewall': firewall_name,
+                'project': project_id,
+            }
+        }
+    
+    delete_firewall_resources = [make_delete_firewall(firewall) for firewall in default_firewalls]    
+    network_dependsOn = [resource['name'] for resource in delete_firewall_resources]
+    delete_network_resource = {
+        'name': '{}-delete-default-network'.format(project_id),
+        'action': 'gcp-types/compute-v1:compute.networks.delete',
         'metadata': {
-            'dependsOn': [app_id]
+            'dependsOn': network_dependsOn
         },
         'properties': {
-            'parent': root_id,
-            'displayName': dev_folder
+                'network': 'default',
+                'project': project_id,
         }
     }
     
-    prd_folder = app_id + '-PRD'
-    folder_prod_resource = {
-        'name': prd_folder,
-        'metadata': {
-            'dependsOn': [app_id]
-        },
-        'type': 'gcp-types/cloudresourcemanager-v2:folders',
-        'properties': {
-            'parent': root_id,
-            'displayName': prd_folder
-        }
-    }
-
-    prd_folder_id = f'$(ref.{prd_folder}.name)'
-    dev_folder_id = f'$(ref.{dev_folder}.name)'
-    # Create dev project
-    dev_project_id = f'{app_id}-project-dev'
-
-    dev_project_resource = {
-        'name': dev_project_id,
-        'type': 'cloudresourcemanager.v1.project',
-        'metadata': {
-            'dependsOn': [
-                dev_folder,
-                prd_folder
-            ]
-        },
-        'properties': {
-            'name': dev_project_id,
-            'projectId': dev_project_id,
-            'parent': {
-                'type': 'folder',
-                'id': dev_folder_id.replace('folders/', '')
-            }
-        }
-    }
-
-    resources = [
-        folder_resource,
-        folder_dev_resource,
-        folder_prod_resource,
-    ]
-
-    return {
-        'resources': resources,
-        'outputs': [
-            {'name': 'dev', 'value': f'$(ref.{dev_folder}.name)'},
-            {'name': 'prd', 'value': f'$(ref.{prd_folder}.name)'},
-        ]
-    }
+    resources = []
+    resources.append(delete_network_resource)
+    resources.extend(delete_firewall_resources)
+    return resources
---
+++ templates/folders.py
@@ -0,0 +1,62 @@
+import copy
+
+def generate_config(ctx):
+    props = ctx.properties
+    app_id = ctx.env['name']
+    parent_node = props['parent_node']
+    # Create Root Folder
+    root_parent_type = parent_node.get('type')
+    root_parent_id = parent_node.get('id')
+    # root_parent = f'{root_parent_type}s/{root_parent_id}'
+    root_parent = '{}s/{}'.format(root_parent_type, root_parent_id)
+
+    folder_resource = {
+        'name': app_id,
+        'type': 'gcp-types/cloudresourcemanager-v2:folders',
+        'properties': {
+            'parent': root_parent,
+            'displayName': app_id
+        }
+    }
+    root_id = '$(ref.{}.name)'.format(app_id)
+
+    # Create Env Folders
+    dev_folder = app_id + '-DEV'
+    folder_dev_resource = {
+        'name': dev_folder,
+        'type': 'gcp-types/cloudresourcemanager-v2:folders',
+        'metadata': {
+            'dependsOn': [app_id]
+        },
+        'properties': {
+            'parent': root_id,
+            'displayName': dev_folder
+        }
+    }
+    
+    prd_folder = app_id + '-PRD'
+    folder_prod_resource = {
+        'name': prd_folder,
+        'metadata': {
+            'dependsOn': [app_id]
+        },
+        'type': 'gcp-types/cloudresourcemanager-v2:folders',
+        'properties': {
+            'parent': root_id,
+            'displayName': prd_folder
+        }
+    }
+
+    resources = [
+        folder_resource,
+        folder_dev_resource,
+        folder_prod_resource,
+    ]
+
+    return {
+        'resources': resources,
+        'outputs': [
+            {'name': 'dev', 'value': '$(ref.{}.name)'.format(dev_folder)},
+            {'name': 'prd', 'value': '$(ref.{}.name)'.format(prd_folder)},
+        ]
+    }
